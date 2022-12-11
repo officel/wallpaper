6f29a8666cc5cdaabcc150aa69171a96730b0574
NISHIMURA Yoshitaka <officel@users.noreply.github.com>
2022-12-11 18:02:13 +0900

    feat: wp.py

---
+++ api/wp.py
@@ -0,0 +1,19 @@
+import base64
+import io
+from http.server import BaseHTTPRequestHandler
+
+from PIL import Image
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        im = Image.open("wp.jpg")
+
+        bytes_io = io.BytesIO()
+        im.save(bytes_io, format="JPEG")
+
+        self.send_response(200)
+        self.send_header("Content-type", "image/jpeg")
+        self.end_headers()
+        self.wfile.write(base64.b64encode(bytes_io.getvalue()).decode("UTF-8"))
+        return
